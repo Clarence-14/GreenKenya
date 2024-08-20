28b4563abdab9c2f88e70bbf5d75af647dec6e46
Clarence-14 <thisisclarence1@gmail.com>
2024-08-20 10:21:48 +0300

    first commit

---
+++ app/src/main/java/com/example/greenkenya/navigation/AppNavHost.kt
@@ -0,0 +1,52 @@
+package com.example.greenkenya.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.greenkenya.ui.theme.screens.about.AboutScreen
+import com.example.greenkenya.ui.theme.screens.home.HomeScreen
+import com.example.greenkenya.ui.theme.screens.login.LoginScreen
+import com.example.greenkenya.ui.theme.screens.signup.SignupScreen
+import com.example.greenkenya.ui.theme.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+
+
+
+
+
+
+    }
+}
